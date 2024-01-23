dd4949c6be8d6b1cc5395f1f9eb58197733b6d7c
xxxsantaklaysxxx <info-mail.ru>
2024-01-23 20:29:07 +0300

    Исправление 1 задания

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name && (name.indexOf(" ") < 0) && name.length >= 4 ? true : false;
+  return name !== null && !name.includes(" ") && name.length >= 4;
 }
 
 function sayHello() {
