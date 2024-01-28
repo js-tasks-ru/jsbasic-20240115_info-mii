f3409a60a94267ed495742cd9f02d840b9305be6
xxxsantaklaysxxx <info-mail.ru>
2024-01-29 00:00:07 +0300

    Задание 2 - Выполнено

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let res = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key]) && salaries[key] !== null && typeof salaries[key] === 'number') {
+      res += salaries[key];
+    }
+  }
+  return res;
+}
+console.log(sumSalary(salaries));
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
+let obj = {};
+let schedule = {};
+obj === schedule;
+
 function isEmpty(obj) {
-  // ваш код...
+  for (let key in obj) {
+    return false;
+  }
+  return true;
 }
+schedule["8:30"] = "подъём";
+console.log(isEmpty(obj));
+alert(isEmpty(schedule));
