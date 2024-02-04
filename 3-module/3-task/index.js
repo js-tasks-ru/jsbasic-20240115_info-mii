eea2051e3238f79f473aa32f1aef4b369aa81a63
xxxsantaklaysxxx <info-mail.ru>
2024-02-04 19:00:52 +0300

    Решение задач 3 модуля

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("");
+  let res;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '-') {
+    arr.splice(i,2,arr[i + 1].toUpperCase());
+    }
+  }
+
+return res = arr.join("")
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
 function showSalary(users, age) {
-  // ваш код...
+    const userFiltered = users.filter(el => el.age <= age)
+    const result = userFiltered.map((el => `${el.name}, ${el.balance}`)).join('\n');
+    return result;
 }
