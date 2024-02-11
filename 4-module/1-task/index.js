c1077d8e32dd7af08e6f9502036d934b157ae72e
xxxsantaklaysxxx <info-mail.ru>
2024-02-11 21:16:58 +0300

    Решение 4 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+  for(let item of friends) {
+    const li = document.createElement('li');
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
+
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+  rows.forEach((row) => {
+  const status = row.querySelector('td[data-available]');
+  if (status && status.getAttribute('data-available') === 'true') {
+          row.classList.add('available');
+        } else if (status && status.getAttribute('data-available') === 'false'){
+          row.classList.add('unavailable');
+        }else {
+          row.setAttribute('hidden', 'true');
+      }
+    const genderCell = row.querySelector('td:nth-child(3)');
+    if(genderCell.textContent === 'm') {
+      row.classList.add('male')
+    }else if((genderCell.textContent === 'f')){
+      row.classList.add('female')
+    }
+     const ageCell = row.querySelector('td:nth-child(2)');
+    if (parseInt(ageCell.textContent) < 18) {
+          row.style.textDecoration = 'line-through';
+      }
+      });
+   
 }
+
